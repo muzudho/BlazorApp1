6063ef5894ae9650da52748fe04bd326132b9b55
muzudho <muzudho1@gmail.com>
2026-02-01 11:27:51 +0900

    WIP: TaskProgress

---
+++ BlazorApp1/Models/ITaskProgress.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorApp1.Models;
+
+/// <summary>
+/// タスクの進捗
+/// </summary>
+public interface ITaskProgress
+{
+
+
+    // ========================================
+    // メソッド
+    // ========================================
+
+
+    void Apply();
+}
---
+++ BlazorApp1/Models/TaskProgress.cs
@@ -0,0 +1,82 @@
+﻿namespace BlazorApp1.Models;
+
+/// <summary>
+/// タスクの進捗
+/// </summary>
+public record TaskProgress : ITaskProgress
+{
+
+
+    // ========================================
+    // プロパティ
+    // ========================================
+
+
+    /// <summary>
+    /// 申請中か
+    /// </summary>
+    public bool IsApplying => this._isAppliying;
+    private bool _isAppliying;
+
+    /// <summary>
+    /// 受理したか
+    /// </summary>
+    public bool IsAccepted => this._isAccepted;
+    private bool _isAccepted;
+
+    /// <summary>
+    /// 実行中か
+    /// </summary>
+    public bool IsUnderExecution => this._isUnderExecution;
+    private bool _isUnderExecution;
+
+    /// <summary>
+    /// 完了か
+    /// </summary>
+    public bool IsCompleted => this._isCompleted;
+    private bool _isCompleted;
+
+
+    // ========================================
+    // 公開メソッド
+    // ========================================
+
+
+    /// <summary>
+    /// 申請します
+    /// </summary>
+    public void Apply()
+    {
+        this._isAppliying = true;
+    }
+
+
+    /// <summary>
+    /// 受理します
+    /// </summary>
+    public void Accept()
+    {
+        this._isAppliying = false;
+        this._isAccepted = true;
+    }
+
+
+    /// <summary>
+    /// 執行します
+    /// </summary>
+    public void Execute()
+    {
+        this._isAppliying = false;
+        this._isUnderExecution = true;
+    }
+
+
+    /// <summary>
+    /// 完了します
+    /// </summary>
+    public void Complete()
+    {
+        this._isUnderExecution = false;
+        this._isCompleted = true;
+    }
+}

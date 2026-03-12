6e8aa5205526d340c8fc19cf7671bd03f75ba500
muzudho <muzudho1@gmail.com>
2026-03-12 20:42:50 +0900

    RValue

---
+++ BlazorApp1/Models/MuzRValue.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorApp1.Models;
+
+/// <summary>
+/// 代入の右辺値（Right-hand side value）。
+/// </summary>
+internal class MuzRValue<T>
+{
+    internal MuzRValue(T? value)
+    {
+        this.Value = value;
+    }
+
+    internal T? Value { get; init; }
+}

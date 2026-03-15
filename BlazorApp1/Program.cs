7f95b50142d8f46a072c716f0ad38df65d0afd56
muzudho <muzudho1@gmail.com>
2026-03-15 11:58:28 +0900

    ホストビルド（＾～＾）

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -1,27 +1,44 @@
 using BlazorApp1.Components;
+using BlazorApp1.Src;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+try
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    // ホストビルドするぜ（＾～＾）！
+    // ［ホスト］ってのは［汎用ホスト］のことで、いろいろ［サービス］っていう便利機能を付け加えることができるフレームワークみたいなもんだぜ（＾～＾）
+    // それを［ビルド］するぜ（＾▽＾）
+    await MuzInfrastructureService.BuildHostAsync(
+        commandLineArgs: args,
+        onHostEnabled: async (host) =>
+        {
+            // ここからビルドされた［汎用ホスト］が使えるぜ（＾▽＾）！
+
+            var builder = WebApplication.CreateBuilder(args);
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error", createScopeForErrors: true);
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+            app.UseHttpsRedirection();
+
+            app.UseAntiforgery();
+
+            app.MapStaticAssets();
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+
+        });
+
 }
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
-
-app.UseAntiforgery();
-
-app.MapStaticAssets();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-app.Run();
+catch (Exception ex)
+{
+    Console.WriteLine($"アプリが死んだ... ログも取れない、むずでょ泣く。{ex}");
+}
---
+++ BlazorApp1/Src/MuzInfrastructureService.cs
@@ -0,0 +1,35 @@
+﻿namespace BlazorApp1.Src;
+
+/// <summary>
+/// どんなコンソール・アプリを作るときでも、本題に入る前に似たようなコードを書くことになる……、そんな似たコード［ホストビルド］をまとめたクラスだぜ（＾～＾）！
+/// </summary>
+public class MuzInfrastructureService
+{
+    public static async Task BuildHostAsync(
+        string[] commandLineArgs,
+        Func<IHost, Task> onHostEnabled)
+    {
+        //HostApplicationBuilder builder = Host.CreateApplicationBuilder(commandLineArgs);  // ビルダー作成（＾～＾）
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(commandLineArgs);  // ビルダー作成（＾～＾）
+
+
+        await SetupBeforeBuildAsync(builder);    // ビルド前の処理（＾～＾）
+        var host = builder.Build(); // ホストビルド（＾～＾）
+        await onHostEnabled(host);  // ホストは有効になっているぜ（＾▽＾）！
+    }
+
+
+    /// <summary>
+    /// ホストビルドする前にやることがあればここでやるぜ（＾～＾）！例えば、［サービス］を追加したりとか、そういうのだぜ（＾～＾）！
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <returns></returns>
+    private static async Task SetupBeforeBuildAsync(WebApplicationBuilder builder)
+    {
+        // お前のアプリケーションに合わせて、［サービス］を追加していってくれだぜ（＾～＾）！
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+    }
+}

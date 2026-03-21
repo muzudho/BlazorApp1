6c5a932beb857445eb3529c75a3e08bc622bad17
muzudho <muzudho1@gmail.com>
2026-03-21 16:20:06 +0900

    wip

--- BlazorApp1/Src/Infrastructure/Study/StudyHelper.cs
+++ BlazorApp1/Src/Infrastructure/Study/StudyHelper.cs
@@ -5,14 +5,39 @@ public static class StudyHelper
     /// <summary>
     /// デッドロックが発生する可能性があり、バッドだが、練習のために使用するタイマー。
     /// </summary>
+    public static void BadSleep(
+        int seconds)
+    {
+        Thread.Sleep(seconds * 1000);   // ここで指定秒かかる処理をしてみる。デッドロックが発生する可能性があり、良くないが、簡単にコードを書けるので使う。
+    }
+
+
+    /// <summary>
+    /// デッドロックが発生する可能性があり、バッドだが、練習のために使用するタイマー。
+    /// </summary>
     public static void BadTimer(
         int seconds,
-        Action onTime)
+        Action onTicked)
     {
         for (int i=0; i<seconds; i++)
         {
             Thread.Sleep(1000);   // ここで何か１秒かかる処理をしてみる。デッドロックが発生する可能性があり、良くないが、簡単にコードを書けるので使う。
-            onTime();
+            onTicked();
+        }
+    }
+
+
+    /// <summary>
+    /// デッドロックが発生する可能性があり、バッドだが、練習のために使用するタイマー。
+    /// </summary>
+    public static async Task TimerAsync(
+        int seconds,
+        Func<Task> onTicked)
+    {
+        for (int i = 0; i < seconds; i++)
+        {
+            await Task.Delay(1000);   // ここで何か１秒かかる処理をしてみる。デッドロックが発生する可能性があり、良くないが、簡単にコードを書けるので使う。
+            await onTicked();
         }
     }
 }

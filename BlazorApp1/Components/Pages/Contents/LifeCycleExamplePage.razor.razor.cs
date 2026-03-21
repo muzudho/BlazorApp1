e8b38fafcf6db99039bae62338f434fc68a16e3c
muzudho <muzudho1@gmail.com>
2026-03-21 13:03:08 +0900

    wip

---
+++ BlazorApp1/Components/Pages/Contents/LifeCycleExamplePage.razor.razor.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorApp1.Components.Pages.Contents;
+
+public partial class LifeCycleExamplePage
+{
+
+
+    // ========================================
+    // 窓口プロパティ
+    // ========================================
+
+
+    private string Message1 { get; set; } = string.Empty;
+    private string Message2 { get; set; } = string.Empty;
+    private string Message3 { get; set; } = string.Empty;
+    private string Message4 { get; set; } = string.Empty;
+    private string Message5 { get; set; } = string.Empty;
+}

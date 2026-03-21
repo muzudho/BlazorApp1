82fa105ac5d6a15d73e7d5eb604b57b9e381b9b6
muzudho <muzudho1@gmail.com>
2026-03-21 15:25:13 +0900

    wip

---
+++ BlazorApp1/Src/Infrastructure/Study/StudyHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorApp1.Src.Infrastructure.Study;
+
+public static class StudyHelper
+{
+    /// <summary>
+    /// デッドロックが発生する可能性があり、バッドだが、練習のために使用するタイマー。
+    /// </summary>
+    public static void BadTimer(
+        int seconds,
+        Action onTime)
+    {
+        Thread.Sleep(seconds * 1000);   // ここで何か時間のかかる処理をしてみる。デッドロックが発生する可能性があり、良くないが、簡単にコードを書けるので使う。
+        onTime();
+    }
+}
